5d73069f562b479eaf054beafd678961d71bae7d
Dhanish Safaya <dhanishsafaya@Dhanish.local>
2023-10-20 23:17:41 +0530

    Profile src issue

---
+++ api/routes/users.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { getUser , updateUser} from "../controllers/user.js";
+
+const router = express.Router()
+
+router.get("/find/:userId", getUser)
+router.put("/", updateUser)
+router.get("/test",(req,res)=>{
+    res.send("it works!!")
+})
+
+
+export default router
